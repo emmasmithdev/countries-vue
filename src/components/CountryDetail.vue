9259cd72ad3f8b0dc3588ecc48742fb6fbc3e4c3
Emma <emmagray183@gmail.com>
2019-12-18 15:16:46 +0000

    inital commit

---
+++ src/components/CountryDetail.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div v-if="country">
+    <h3>{{country.name}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "country-detail",
+  props: ["country"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/CountryList.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div>
+
+      <!-- <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item> -->
+      <label for="country_select">Select a Country:</label>
+          <select id="country_select" @change="handleChange">
+            <option disabled value="">Select a country</option>
+            <option v-for="(country, index) in countries" :country="country" :key="index">{{country.name}}</option>
+          </select>
+  </div>
+</template>
+
+<script>
+  import ListItem from './ListItem.vue';
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-item": ListItem
+  },
+  methods: {
+    handleChange(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
